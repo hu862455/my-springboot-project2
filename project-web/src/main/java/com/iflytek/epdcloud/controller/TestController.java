8cf3972b141b8b53efd2f9de5cd7235dd7355b1a
hushuai <hellohushua@gmail.com>
2019-02-13 09:54:40 +0800

    Revert "集成swagger2"
    This reverts commit 1d809aca

--- project-web/src/main/java/com/iflytek/epdcloud/controller/TestController.java
+++ project-web/src/main/java/com/iflytek/epdcloud/controller/TestController.java
@@ -1,13 +1,9 @@
 package com.iflytek.epdcloud.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.iflytek.epdcloud.service.DemoService;
 /**
@@ -20,19 +16,16 @@ import com.iflytek.epdcloud.service.DemoService;
  **/
 @Controller
 @RequestMapping("demo")
-@Api("swaggerDemo")
 public class TestController {
     @Autowired
     private DemoService demoService;
 
-    @ApiOperation(value="测试接口", notes="获取测试用户")
-    @ApiImplicitParam(name = "空", value = "空", required = true, dataType = "空")
     @GetMapping("test")
     public String test() {
         return demoService.test();
     }
 
-    @RequestMapping(value = "index" ,method = RequestMethod.GET)
+    @RequestMapping("index")
     public String index(){
         return "index";
     }
